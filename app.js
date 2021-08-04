4264f595a62f7c4709dbf38364dc68f70a4a341d
mreshtaiwi <mr.eshtaiwi@gmail.com>
2021-08-05 00:46:42 +0300

    starter code

--- app.js
+++ app.js
@@ -4,7 +4,6 @@
 Write a function called sum() that takes in two numbers as arguments and then returns an array where the first element is the sum of those numbers, and the second element is a concatenated string that EXACTLY follows this example and uses the values that were input into the function:
 
 "The sum of 4 and 7 is 11."
-
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSum() function below and check the console to see if the test passes.*/
 
 // Write your code here
@@ -28,7 +27,6 @@ function multiply(a, b) {
 
 // Here is the test for multiply(); uncomment it to run it
 // testMultiply(5,9);
-
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
 /////////////////////////////////////
